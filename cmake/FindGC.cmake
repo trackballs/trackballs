c7c553319f980e35ef86448e75683620681ee743
Manuel Stoeckl <mstoeckl@users.noreply.github.com>
2017-06-19 19:54:28 -0400

    CMake searches for libgc since it is linked along with Guile
    Both Guile 2.0 and Guile 2.2 indicate that one should link
    with and include headers for libgc as well.

---
+++ cmake/FindGC.cmake
@@ -0,0 +1,23 @@
+# Locate libgc (BDW-gc)
+
+# PkgConfig is only there to provide hints
+find_package(PkgConfig)
+pkg_check_modules(PC_GC QUIET bdw-gc)
+
+set(GC_DEFINITIONS ${PC_GC_CFLAGS_OTHER})
+
+# Former needed by Guile2.0, latter by 2.2
+find_path(GC_INCLUDE_DIR NAMES gc/gc.h gc/gc_inline.h
+          HINTS ${PC_GC_INCLUDEDIR} ${PC_GC_INCLUDE_DIRS})
+
+find_library(GC_LIBRARY NAMES gc
+             HINTS ${PC_GC_LIBDIR} ${PC_GC_LIBRARY_DIRS} )
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(libgc REQUIRED_VARS
+                                  GC_LIBRARY GC_INCLUDE_DIR
+                                  VERSION_VAR PC_GC_VERSION)
+
+set(GC_LIBRARIES ${GC_LIBRARY} )
+set(GC_INCLUDE_DIRS ${GC_INCLUDE_DIR} )
+
