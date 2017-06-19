b0fa5385eb0d9f84bb991a2a8a9c4e3ecbbf9248
Manuel Stoeckl <mstoeckl@users.noreply.github.com>
2017-06-19 18:21:13 -0400

    SDL and Guile now correctly searched for by CMake
    Rewrote FindGuile from scratch because the old version failed
    on some systems.

--- cmake/FindGuile.cmake
+++ cmake/FindGuile.cmake
@@ -1,101 +1,34 @@
-# Copyright (c) 2008, 2014 OpenCog.org (http://opencog.org)
-#
-# Redistribution and use is allowed according to the terms of the BSD license.
-# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+# Locate Guile. 
+# Note: `guile-config` ultimately calls pkg-config anyway
+# Nothing gets marked `advanced` since there aren't that many variables
 
-# - Try to find Guile; Once done this will define
-#
-# GUILE_FOUND            - system has the GUILE library
-# GUILE_INCLUDE_DIRS     - the GUILE include directory
-# GUILE_LIBRARIES        - The libraries needed to use GUILE
-# GUILE_VERSION_STRING   - Version
-# GUILE_SITE_DIR         - site dir
-# GUILE_EXTENSION_DIR    - extension dir
-# GUILE_ROOT_DIR         - prefix dir
+find_program(GUILE_SNARF NAMES guile-snarf guile-snarf2.2 guile-snarf2.0)
 
-# Look for the header file
-# Look for guile-2.2 first, then 2.0, then 1.8
-# Macports for OSX puts things in /opt/local
-find_path (GUILE_INCLUDE_DIR libguile.h
-  PATH_SUFFIXES
-    guile/2.2
-    guile/2.0
-    guile/1.8
-    libguile
-    guile
-  HINTS /opt/local/include
-)
-
-# Look for the library
-find_library (GUILE_LIBRARY NAMES guile-2.2 guile-2.0 guile
-  HINTS
-    /opt/local/lib
-)
+# PkgConfig is only there to provide hints
+find_package(PkgConfig)
+pkg_check_modules(PC_GUILE QUIET guile)
+if (NOT PC_GUILE_FOUND)
+  pkg_check_modules(PC_GUILE QUIET guile-2.2)
+  if (NOT PC_GUILE_FOUND)
+    pkg_check_modules(PC_GUILE QUIET guile-2.0)
+  endif(NOT PC_GUILE_FOUND)
+endif(NOT PC_GUILE_FOUND)
 
 
-set (GUILE_LIBRARIES ${GUILE_LIBRARY})
-set (GUILE_INCLUDE_DIRS ${GUILE_INCLUDE_DIR})
+set(GUILE_DEFINITIONS ${PC_GUILE_CFLAGS_OTHER})
 
+find_path(GUILE_INCLUDE_DIR libguile.h
+          HINTS ${PC_GUILE_INCLUDEDIR} ${PC_GUILE_INCLUDE_DIRS}
+          PATH_SUFFIXES guile guile/2.2 guile/2.0)
 
-# check guile's version if we're using cmake >= 2.6
-if (GUILE_INCLUDE_DIR)
-  SET(GUILE_VERSION_MAJOR 0)
-  SET(GUILE_VERSION_MINOR 0)
-  SET(GUILE_VERSION_PATCH 0)
+find_library(GUILE_LIBRARY NAMES guile guile-2.2 guile-2.0
+             HINTS ${PC_GUILE_LIBDIR} ${PC_GUILE_LIBRARY_DIRS} )
 
-  IF(NOT EXISTS "${GUILE_INCLUDE_DIR}/libguile/version.h")
-          MESSAGE(FATAL_ERROR "Found ${GUILE_INCLUDE_DIR}/libguile.h but not version.h; check your guile installation!")
-  ENDIF(NOT EXISTS "${GUILE_INCLUDE_DIR}/libguile/version.h")
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(libguile REQUIRED_VARS
+                                  GUILE_LIBRARY GUILE_INCLUDE_DIR
+                                  VERSION_VAR PC_GUILE_VERSION)
 
-  # Extract the libguile version from the 'version.h' file
-  SET(GUILE_MAJOR_VERSION 0)
-  FILE(READ "${GUILE_INCLUDE_DIR}/libguile/version.h" _GUILE_VERSION_H_CONTENTS)
+set(GUILE_LIBRARIES ${GUILE_LIBRARY} )
+set(GUILE_INCLUDE_DIRS ${GUILE_INCLUDE_DIR} )
 
-  STRING(REGEX MATCH "#define SCM_MAJOR_VERSION[	 ]+([0-9])" _MATCH "${_GUILE_VERSION_H_CONTENTS}")
-  SET(GUILE_VERSION_MAJOR ${CMAKE_MATCH_1})
-  STRING(REGEX MATCH "#define SCM_MINOR_VERSION[	 ]+([0-9]+)" _MATCH "${_GUILE_VERSION_H_CONTENTS}")
-  SET(GUILE_VERSION_MINOR ${CMAKE_MATCH_1})
-  STRING(REGEX MATCH "#define SCM_MICRO_VERSION[	 ]+([0-9]+)" _MATCH "${_GUILE_VERSION_H_CONTENTS}")
-  SET(GUILE_VERSION_PATCH ${CMAKE_MATCH_1})
-
-  SET(GUILE_VERSION_STRING "${GUILE_VERSION_MAJOR}.${GUILE_VERSION_MINOR}.${GUILE_VERSION_PATCH}")
-
-endif ()
-
-find_program(GUILE_EXECUTABLE
-              NAMES guile
-           )
-
-find_program(GUILE_CONFIG_EXECUTABLE
-              NAMES guile-config
-           )
-
-
-if (GUILE_CONFIG_EXECUTABLE)
-  execute_process (COMMAND ${GUILE_CONFIG_EXECUTABLE} info prefix
-                    OUTPUT_VARIABLE GUILE_ROOT_DIR
-                    OUTPUT_STRIP_TRAILING_WHITESPACE)
-
-  execute_process (COMMAND ${GUILE_CONFIG_EXECUTABLE} info sitedir
-                    OUTPUT_VARIABLE GUILE_SITE_DIR
-                    OUTPUT_STRIP_TRAILING_WHITESPACE)
-
-  execute_process (COMMAND ${GUILE_CONFIG_EXECUTABLE} info extensiondir
-                    OUTPUT_VARIABLE GUILE_EXTENSION_DIR
-                    OUTPUT_STRIP_TRAILING_WHITESPACE)
-endif ()
-
-# IF(GUILE_FOUND AND GUILE_VERSION_MAJOR EQUAL 2)
-# 	ADD_DEFINITIONS(-DHAVE_GUILE2)
-# ENDIF(GUILE_FOUND AND GUILE_VERSION_MAJOR EQUAL 2)
-
-# handle REQUIRED and QUIET options
-include (FindPackageHandleStandardArgs)
-if (CMAKE_VERSION LESS 2.8.3)
-  find_package_handle_standard_args (Guile DEFAULT_MSG GUILE_ROOT_DIR GUILE_INCLUDE_DIRS GUILE_LIBRARIES GUILE_VERSION_STRING)
-else ()
-  find_package_handle_standard_args (Guile REQUIRED_VARS GUILE_ROOT_DIR GUILE_INCLUDE_DIRS GUILE_LIBRARIES VERSION_VAR GUILE_VERSION_STRING)
-endif ()
-
-
-MARK_AS_ADVANCED(GUILE_INCLUDE_DIR GUILE_LIBRARY)
---
+++ cmake/FindSDL2_and_friends.cmake
@@ -0,0 +1,57 @@
+# Locate SDL2, SDL2_image, SDL_mixer, and SDL_ttf
+# Nothing gets marked `advanced` since there aren't that many variables
+
+# PkgConfig is only there to provide hints
+find_package(PkgConfig)
+pkg_check_modules(PC_SDL2 QUIET sdl2)
+pkg_check_modules(PC_SDL2_IMAGE QUIET SDL2_image)
+pkg_check_modules(PC_SDL2_MIXER QUIET SDL2_mixer)
+pkg_check_modules(PC_SDL2_TTF QUIET SDL2_ttf)
+
+set(SDL2_DEFINITIONS ${PC_SDL2_CFLAGS_OTHER} ${PC_SDL2_IMAGE_CFLAGS_OTHER}
+            ${PC_SDL2_MIXER_CFLAGS_OTHER} ${PC_SDL2_TTF_CFLAGS_OTHER})
+
+find_path(SDL2_INCLUDE_DIR SDL.h
+          HINTS ${PC_SDL2_INCLUDEDIR} ${PC_SDL2_INCLUDE_DIRS}
+          PATH_SUFFIXES SDL2 )
+find_path(SDL2_IMAGE_INCLUDE_DIR SDL2/SDL_image.h
+          HINTS ${PC_SDL2_IMAGE_INCLUDEDIR} ${PC_SDL2_IMAGE_INCLUDE_DIRS}
+          PATH_SUFFIXES SDL2 )
+find_path(SDL2_MIXER_INCLUDE_DIR SDL2/SDL_mixer.h
+          HINTS ${PC_SDL2_MIXER_INCLUDEDIR} ${PC_SDL2_MIXER_INCLUDE_DIRS}
+          PATH_SUFFIXES SDL2 )
+find_path(SDL2_TTF_INCLUDE_DIR SDL2/SDL_ttf.h
+          HINTS ${PC_SDL2_TTF_INCLUDEDIR} ${PC_SDL2_TTF_INCLUDE_DIRS}
+          PATH_SUFFIXES SDL2 )
+
+find_library(SDL2_LIBRARY NAMES SDL2 libSDL2
+             HINTS ${PC_SDL2_LIBDIR} ${PC_SDL2_LIBRARY_DIRS} )
+find_library(SDL2_IMAGE_LIBRARY NAMES SDL2_image libSDL2_image
+             HINTS ${PC_SDL2_IMAGE_LIBDIR} ${PC_SDL2_IMAGE_LIBRARY_DIRS} )
+find_library(SDL2_MIXER_LIBRARY NAMES SDL2_mixer libSDL2_mixer
+             HINTS ${PC_SDL2_MIXER_LIBDIR} ${PC_SDL2_MIXER_LIBRARY_DIRS} )
+find_library(SDL2_TTF_LIBRARY NAMES SDL2_ttf libSDL2_ttf
+             HINTS ${PC_SDL2_TTF_LIBDIR} ${PC_SDL2_TTF_LIBRARY_DIRS} )
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(libSDL2  REQUIRED_VARS
+                                  SDL2_LIBRARY SDL2_INCLUDE_DIR
+                                  VERSION_VAR PC_SDL2_VERSION)
+find_package_handle_standard_args(libSDL2_image  REQUIRED_VARS
+                                  SDL2_IMAGE_LIBRARY SDL2_IMAGE_INCLUDE_DIR
+                                  VERSION_VAR PC_SDL2_IMAGE_VERSION)
+find_package_handle_standard_args(libSDL2_mixer  REQUIRED_VARS
+                                  SDL2_MIXER_LIBRARY SDL2_MIXER_INCLUDE_DIR
+                                  VERSION_VAR PC_SDL2_MIXER_VERSION)
+find_package_handle_standard_args(libSDL2_ttf  REQUIRED_VARS
+                                  SDL2_TTF_LIBRARY SDL2_TTF_INCLUDE_DIR
+                                  VERSION_VAR PC_SDL2_TTF_VERSION)
+
+set(SDL2_LIBRARIES ${SDL2_LIBRARY} )
+set(SDL2_IMAGE_LIBRARIES ${SDL2_IMAGE_LIBRARY} )
+set(SDL2_MIXER_LIBRARIES ${SDL2_MIXER_LIBRARY} )
+set(SDL2_TTF_LIBRARIES ${SDL2_TTF_LIBRARY} )
+set(SDL2_INCLUDE_DIRS ${SDL2_INCLUDE_DIR} )
+set(SDL2_IMAGE_INCLUDE_DIRS ${SDL2_IMAGE_INCLUDE_DIR} )
+set(SDL2_MIXER_INCLUDE_DIRS ${SDL2_MIXER_INCLUDE_DIR} )
+set(SDL2_TTF_INCLUDE_DIRS ${SDL2_TTF_INCLUDE_DIR} )
